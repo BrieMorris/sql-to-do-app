7aae4a688d5538e00101e6b66c7ba2c31f2f21a2
Brie Morris <morris.brie@gmail.com>
2023-08-12 13:39:02 -0500

    added return

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,63 @@
-import {useState} from 'react';
+import {useState,  useEffect} from 'react';
+import axios from 'axios';
+
+let toDoData = [];
 
 function App () {
-  
+  const [toDoList, setToDoList] = useState(toDoData);
+  const [newTask, setNewTask] =
+  useState('');
+  const [completeTask, setCompleteTask] =
+  useState('');
+
+  const fetchList = () => {
+    axios.get('/list')
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      setToDoList(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const addList = (event) => {
+    event.preventDefault();
+    axios.post(`/list`, {task: newTask, complete: completeTask})
+    .then( (response) => {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
+ 
   return (
     <div>
       <h1>TO DO APP</h1>
+
+      <form onSubmit={addList}>
+        <label>Task</label>
+        <input onChange={ (event) => setNewTask(event.target.value)}   />
+        <label>Complete</label>
+        <input onChange={ (event) => setCompleteTask(event.target.value)} />
+        <button type="submit">Add Task</button>
+      </form>
+
+      {toDoList.map(list =>
+      (<li key={list.task}>
+        {list.task} is {list.complete}
+      </li>
+      ))}
+
     </div>
   );
 
 }
-
+// above add ul? what will make a bullet point 
 export default App
